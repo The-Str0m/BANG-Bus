2a21ccd83a5ee1b95282c13c79967812e19b0271
The-Str0m <pranavsagiraju@gmail.com>
2025-11-23 13:00:11 +0530

    Create disclosure.js

---
+++ bangbus/disclosure.js
@@ -0,0 +1,10 @@
+class name {
+    constructor(parameters) {
+        // This Demo site was made using HTML java and CSS. We used AI for minor changes and diffcult programs in sinippets here and there
+        //Styles CSS is almost 60% ai beacuse its very time consuming to type in colour codes and manually enter the values 
+        //theme Manager uses some AI genrated scripts to allow for seamless theme changing 
+        // app JS has multiple small AI lines of code, for example the hover effect and hightlighting  
+        // main LLM USED Was CHAT GPT 
+        
+    }
+}
